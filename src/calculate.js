d4860dd0450787d9e6ad6bbd2897c0b62f755b72
Artem <a.ku666@vk.com>
2023-07-04 20:59:16 +0200

    init

---
+++ src/calculate.js
@@ -0,0 +1,9 @@
+export function calculateTotal (purchases, applyDiscount) {
+    let sum = purchases.reduce((total, item) => {
+        return total + (item.count * item.price);
+    }, 0);
+    if (applyDiscount) {
+        return sum * 0.5;
+    }
+    return sum
+}
